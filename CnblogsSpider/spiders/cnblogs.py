35fb7b985bf732ed6c15fed1e800ffb1f3b2e70a
anmutu <anmutu@hotmail.com>
2020-02-21 00:45:58 +0800

    fix image_url提取时的bug

--- CnblogsSpider/spiders/cnblogs.py
+++ CnblogsSpider/spiders/cnblogs.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from urllib import parse
-
+import json
 
 import scrapy
 from scrapy import Request
+import requests
+import re
+
 
 
 class CnblogsSpider(scrapy.Spider):
@@ -14,8 +17,8 @@ class CnblogsSpider(scrapy.Spider):
     def parse(self, response):
         post_nodes = response.css('#post_list  .post_item')
         for post_node in post_nodes:
-            image_url = post_node.css('.post_item_summary a img::attr(href)').exact_first("")
-            post_url = post_node.css('h3::attr(href)').exact_first("")
+            image_url = post_node.css('.post_item_summary a img::attr(href)').extract_first("")
+            post_url = post_node.css('h3::attr(href)').extract_first("")
             yield Request(url=parse.urljoin(response.url, post_url), meta={"front_image_url": image_url},
                           callback=self.parse_detail)
 
@@ -26,12 +29,18 @@ class CnblogsSpider(scrapy.Spider):
             next_url = response.css('div.pager a:last-child::attr(href)').extract_first("")
             yield Request(url=parse.urljoin(response.url, next_url))
 
-
-    def parse_detail(self, response):
+    def parse_detail(self,  response):
         title = response.css('#news_title a::text').extract_first("")
         publish_time = response.css('#news_info .time::text').extract_first("")
         content = response.css('#news_content').extract("")[0]
         tag_list = response.css('.news_tags a::text').extract()
 
-
+        match_re=re.match(".*?(\d+)", response.url)
+        if match_re:
+            post_id = match_re.group(1)
+            html = requests.get(parse.urljoin(response.url, "/NewsAjax/GetAjaxNewsInfo?contentId={}".format(post_id)))
+            j_data = json.load(html.text)
+            like_nums =j_data["DiggCount"]  # 点赞数
+            view_nums = j_data["TotalView"]  # 阅读数
+            comment_nums = j_data["CommentCount"]  # 评论数
         pass
