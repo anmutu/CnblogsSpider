a3b8a3331855c22be707d75db3563367a4e2ce20
anmutu <anmutu@hotmail.com>
2020-02-21 16:36:37 +0800

    生成md5的公共函数。

---
+++ CnblogsSpider/util/common.py
@@ -0,0 +1,22 @@
+# _*_ coding :utf-8 _*_
+__author__ = 'du'
+__bolg__ = 'www.github.com/anmutu;'
+__date__ = '2020/2/21 16:28'
+
+import hashlib
+
+
+def get_md5(url):
+    if isinstance(url, str):
+        url = url.encode("utf-8")
+    md5 = hashlib.md5()
+    md5.update(url)
+    return md5.hexdigest()
+
+
+if __name__ == "__main__":
+    print(get_md5("www.cnblogs.com"))
+
+
+
+
